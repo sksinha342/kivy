cfe8d1a03e1ecd9ddfd3ac4de3bb7a534ffcf9c7
Sujit Kushwaha <kumarmai00123@gmail.com>
2026-02-05 10:09:36 +0530

    nothing

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ class HelloWorld(App):
     def build(self):
         return Button(text='Hello Mobile World!', 
                       font_size='20sp',
-                      background_color=(0, 1, 0, 1))
+                      background_color=(0, 1, 0, 0.7))
 
 if __name__ == '__main__':
     HelloWorld().run()
